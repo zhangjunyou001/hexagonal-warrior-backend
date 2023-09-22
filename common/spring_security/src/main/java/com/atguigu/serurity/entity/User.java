ac491357afb5bd7b288d12e626c898a31b380a83
Jason04138 <zhenxing1521@163.com>
2023-09-22 15:21:03 +0800

    enhance codes

--- common/spring_security/src/main/java/com/atguigu/serurity/entity/User.java
+++ common/spring_security/src/main/java/com/atguigu/serurity/entity/User.java
@@ -1,7 +1,6 @@
 package com.atguigu.serurity.entity;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,19 +11,14 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty(value = "微信openid")
 	private String username;
 
-	@ApiModelProperty(value = "密码")
 	private String password;
 
-	@ApiModelProperty(value = "昵称")
 	private String nickName;
 
-	@ApiModelProperty(value = "用户头像")
 	private String salt;
 
-	@ApiModelProperty(value = "用户签名")
 	private String token;
 
 }
