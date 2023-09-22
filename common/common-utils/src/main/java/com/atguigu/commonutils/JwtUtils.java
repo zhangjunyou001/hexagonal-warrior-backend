800ed7aed52e7901622cdedd331984c79fd72022
Jason04138 <zhenxing1521@163.com>
2023-09-22 15:22:29 +0800

    enhance util

--- common/common-utils/src/main/java/com/atguigu/commonutils/JwtUtils.java
+++ common/common-utils/src/main/java/com/atguigu/commonutils/JwtUtils.java
@@ -4,15 +4,14 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 public class JwtUtils {
-    public static final long EXPIRE = 1000 * 60 * 60 * 24; //token过期时间
-    public static final String APP_SECRET = "ukc8BDb8888RigUDaY6HOHYUhelloworld"; //秘钥
+    public static final long EXPIRE = 1000 * 60 * 60 * 24;
+    public static final String APP_SECRET = "ukc8BDb8888RigUDaY6HOHYUhelloworld";
 
     public static String getJwtToken(String id, String nickname){
 
@@ -24,7 +23,7 @@ public class JwtUtils {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
 
-                .claim("id", id)  //设置token主体部分 ，存储用户信息
+                .claim("id", id)
                 .claim("nickname", nickname)
 
                 .signWith(SignatureAlgorithm.HS256, APP_SECRET)
